8c9638ca89f57da4a5aad44833b16fd391ceea2f
eamondo2 <eamonbb@gmail.com>
2014-09-17 16:30:19 -0400

    second initial commit. Still no actual things done yet.

--- src/com/shit/minecraft/ShitMinecraft.java
+++ src/com/shit/minecraft/ShitMinecraft.java
@@ -4,6 +4,13 @@ package com.shit.minecraft;
  * Created by eamon_000 on 9/17/2014.
  */
 public class ShitMinecraft {
+	public static void main(String[] args){
+		System.out.println("Hello World");
 
-	
+
+
+
+
+	}
+
 }
