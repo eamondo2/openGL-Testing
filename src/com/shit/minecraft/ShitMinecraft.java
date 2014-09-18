f2e63dfd16b63c7798e85751e131f25bebbc5141
eamondo2 <eamonbb@gmail.com>
2014-09-17 21:29:15 -0400

    adding skeleton

--- src/com/shit/minecraft/ShitMinecraft.java
+++ src/com/shit/minecraft/ShitMinecraft.java
@@ -2,6 +2,7 @@ package com.shit.minecraft;
 
 /**
  * Created by eamon_000 on 9/17/2014.
+ * credit to Eamon, Adam, Reuben, Zach
  */
 public class ShitMinecraft {
 	public static void main(String[] args){
@@ -12,5 +13,34 @@ public class ShitMinecraft {
 
 
 	}
+    //initializes all openGL functions, sets display mode, and enables depth buffers etc
+    public static void initGL(){
+
+
+    }
+    //initializes display, sets the screen size, and grabs the mouse
+    public static void init(){
+
+
+    }
+    //contains main loop as well as the logic processes and ensuring that the application doesn't take all of the cpu cycles when in the background
+    public static void run(){
+
+
+    }
+    //calls the logical functions, calculating look position, change in movement
+    //also calculates physics (eventually) and generally does all the heavy lifting
+    //most of this will be calls to other functions
+    public static void logic(){
+
+
+    }
+    //self explanatory. update display, render out shapes etc.
+    //will have two render modes, 2d and 3d, for the menus and the general world
+    //also calls many other external functions to render out the scene
+    public static void render(){
+
+
+    }
 
 }
