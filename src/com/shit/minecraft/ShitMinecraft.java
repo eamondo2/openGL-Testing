edae0e33b820b0030181e82d3f8429a852af4c6a
eamondo2 <eamonbb@gmail.com>
2014-09-29 14:40:57 -0400

    error fixin

--- src/com/shit/minecraft/ShitMinecraft.java
+++ src/com/shit/minecraft/ShitMinecraft.java
@@ -5,7 +5,6 @@ import org.lwjgl.LWJGLException;
 import org.lwjgl.input.Keyboard;
 import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.Display;
-import org.lwjgl.opengl.DisplayMode;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.util.vector.Vector3f;
 
@@ -21,6 +20,8 @@ import static org.lwjgl.util.glu.GLU.gluPerspective;
  */
 public class ShitMinecraft {
     public static boolean closeRequested = false;
+    public static boolean renderingModeIs2D = false;
+
     public static ArrayList<Projectile> projectiles = new ArrayList<Projectile>();
     public static float FOV = 50f;
     public static float angle = 0f;
@@ -39,13 +40,35 @@ public class ShitMinecraft {
 
 	}
 
+    public static void enable3D() {
+        if (renderingModeIs2D) {
+            glEnable(GL_DEPTH_TEST);
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            gluPerspective(70.0f, Display.getWidth() / Display.getHeight(), 0.1f, 10000.0f);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+            renderingModeIs2D = false;
+        }
+    }
+
+    public static void enable2D() {
+        if (!renderingModeIs2D) {
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            glOrtho(0, Display.getWidth(), Display.getHeight(), 0, -1, 1);
+            glMatrixMode(GL_MODELVIEW);
+            glLoadIdentity();
+            renderingModeIs2D = true;
+        }
+    }
     public static void cleanup() {
         Display.destroy();
     }
 
     public static void waitabit() {
         try {
-            Thread.sleep(10);
+            Thread.sleep(30);
 
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -71,7 +94,7 @@ public class ShitMinecraft {
     //initializes display, sets the screen size, and grabs the mouse
     public static void init(){
         try {
-            Display.setDisplayMode(new DisplayMode(640, 400));
+            //Display.setDisplayMode(new DisplayMode(640, 400));
             Display.setTitle("ShittyMinecraft");
 
             Display.create();
@@ -100,8 +123,7 @@ public class ShitMinecraft {
 
                 logic();
 
-                if (!isPaused) render();
-                else menuRender();
+                //render();
 
 
             } else {
@@ -120,8 +142,8 @@ public class ShitMinecraft {
 
                 } else {
                     logic();
-                    if (!isPaused) render();
-                    else menuRender();
+                    //render();
+
                 }
             }
 
@@ -132,9 +154,7 @@ public class ShitMinecraft {
     //calls the logical functions, calculating look position, change in movement
     //also calculates physics (eventually) and generally does all the heavy lifting
     //most of this will be calls to other functions
-    public static void menuRender() {
 
-    }
 
     public static void pausedInput() {
         if (Keyboard.isKeyDown(Keyboard.KEY_P)) {
@@ -178,6 +198,7 @@ public class ShitMinecraft {
             isPaused = !isPaused;
             waitabit();
         }
+
     }
 
     public static void logic() {
@@ -186,15 +207,14 @@ public class ShitMinecraft {
             camera();
             input();
             projectileLogic();
+            render();
 
         } else {
             pausedInput();
             menuLogic();
+            //menurender()
 
         }
-        //not important code
-        if (angle < 360) angle += 1f;
-        else angle = 0;
 
 
     }
@@ -239,6 +259,16 @@ public class ShitMinecraft {
     //also calls many other external functions to render out the scene
 
     public static void render(){
+        //2d Rendering section
+        //enable2D();
+
+
+        enable3D();
+
+
+        //not important code
+        if (angle < 360) angle += 1f;
+        else angle = 0;
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         for (Projectile p : projectiles) {
             p.render();
@@ -338,6 +368,10 @@ public class ShitMinecraft {
         glVertex3f(0f, 0f, 10f);
         glEnd();
         glPopMatrix();
+        enable2D();
+
+
+
     }
 
 }
---
+++ src/com/shit/minecraft/util/satUtils.java
@@ -0,0 +1,9 @@
+package com.shit.minecraft.util;
+
+/**
+ * Created by eamondo2 on 9/29/14.
+ */
+public class satUtils {
+
+
+}
