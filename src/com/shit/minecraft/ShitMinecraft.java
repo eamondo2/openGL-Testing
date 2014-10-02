d449c196f4d7e7ddf39a626571d7cf8887e3f5d0
Reuben <rfglosson@gmail.com>
2014-10-02 11:38:07 -0400

    nil

--- src/com/shit/minecraft/ShitMinecraft.java
+++ src/com/shit/minecraft/ShitMinecraft.java
@@ -37,7 +37,7 @@ public class ShitMinecraft {
     public static void main(String[] args){
 		System.out.println("Hello Woldr");
         System.out.println("Reuben can't spell...");
-        System.out.println("Zach forgot a semicolon");
+        System.out.println("Zach forgot a semicolon shitwankers");
         
         run();
         cleanup();
